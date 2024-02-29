c45cd2d7e87b84e7c7e2d8261d240b3ed1e06892
Biplob Dash <110329225+BiplobDash@users.noreply.github.com>
2024-02-29 09:07:14 +0600

    Firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:main_project/firebase_options.dart';
 import 'package:main_project/pages/question.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
