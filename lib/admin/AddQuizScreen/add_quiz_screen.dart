3b0348242ec24662229eea8f4a7e77657616b031
Biplob Dash <110329225+BiplobDash@users.noreply.github.com>
2024-03-04 23:16:56 +0600

    Login Function Complete

---
+++ lib/admin/AddQuizScreen/add_quiz_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddQuizScreen extends StatefulWidget {
+  const AddQuizScreen({super.key});
+
+  @override
+  State<AddQuizScreen> createState() => _AddQuizScreenState();
+}
+
+class _AddQuizScreenState extends State<AddQuizScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
--- lib/admin/admin_login.dart
+++ lib/admin/admin_login.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:main_project/admin/AddQuizScreen/add_quiz_screen.dart';
 
 class AdminLogin extends StatefulWidget {
   const AdminLogin({super.key});
@@ -14,102 +16,89 @@ class _AdminLoginState extends State<AdminLogin> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFededeb),
-      body: Stack(
-        children: [
-          Container(
-            margin:
-                EdgeInsets.only(top: MediaQuery.sizeOf(context).height / 2),
-            padding: const EdgeInsets.only(top: 45, left: 20, right: 20),
-            height: MediaQuery.sizeOf(context).height,
-            width: MediaQuery.sizeOf(context).width,
-            decoration: BoxDecoration(
-              gradient: const LinearGradient(
-                colors: [
-                  Color.fromARGB(255, 53, 51, 51),
-                  Colors.black,
-                ],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              ),
-              borderRadius: BorderRadius.vertical(
-                top: Radius.elliptical(
-                  MediaQuery.sizeOf(context).width,
-                  110,
-                ),
+      body: buildUI(),
+    );
+  }
+
+  Widget buildUI() {
+    return Stack(
+      children: [
+        Container(
+          margin: EdgeInsets.only(top: MediaQuery.sizeOf(context).height / 2),
+          padding: const EdgeInsets.only(top: 45, left: 20, right: 20),
+          height: MediaQuery.sizeOf(context).height,
+          width: MediaQuery.sizeOf(context).width,
+          decoration: BoxDecoration(
+            gradient: const LinearGradient(
+              colors: [
+                Color.fromARGB(255, 53, 51, 51),
+                Colors.black,
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            borderRadius: BorderRadius.vertical(
+              top: Radius.elliptical(
+                MediaQuery.sizeOf(context).width,
+                110,
               ),
             ),
           ),
-          Container(
-            margin: const EdgeInsets.only(left: 30, right: 30, top: 60),
-            child: Form(
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    const Text(
-                      'Let\'s start with Admin',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
-                        fontSize: 25,
+        ),
+        Container(
+          margin: const EdgeInsets.only(left: 30, right: 30, top: 60),
+          child: Form(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  const Text(
+                    'Let\'s start with Admin',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      fontSize: 25,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Material(
+                    elevation: 3.0,
+                    borderRadius: BorderRadius.circular(20),
+                    child: Container(
+                      height: MediaQuery.sizeOf(context).height / 2.2,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Column(
+                        children: [
+                          const SizedBox(
+                            height: 50,
+                          ),
+                          _textFeild('UserName', userNameController,
+                              'Please enter your Username'),
+                          const SizedBox(
+                            height: 40,
+                          ),
+                          _textFeild('Password', passController,
+                              'Please enter your Password'),
+                          const SizedBox(
+                            height: 40,
+                          ),
+                          _buttonWidget(() {
+                            loginAdmin();
+                          }),
+                        ],
                       ),
                     ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    Material(
-                      elevation: 3.0,
-                      borderRadius: BorderRadius.circular(20),
-                      child: Container(
-                        height: MediaQuery.sizeOf(context).height / 2.2,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: Column(
-                          children: [
-                            const SizedBox(
-                              height: 50,
-                            ),
-                            _textFeild('UserName', userNameController,
-                                'Please enter your Username'),
-                            const SizedBox(
-                              height: 40,
-                            ),
-                            _textFeild('Password', passController,
-                                'Please enter your Password'),
-                            const SizedBox(
-                              height: 40,
-                            ),
-                            Container(
-                              padding: const EdgeInsets.symmetric(vertical: 12),
-                              margin:
-                                  const EdgeInsets.symmetric(horizontal: 20),
-                              width: MediaQuery.sizeOf(context).width,
-                              decoration: BoxDecoration(
-                                  color: Colors.black,
-                                  borderRadius: BorderRadius.circular(10)),
-                              child: const Center(
-                                child: Text(
-                                  'Login',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
-          )
-        ],
-      ),
+          ),
+        )
+      ],
     );
   }
 
@@ -119,10 +108,11 @@ class _AdminLoginState extends State<AdminLogin> {
       padding: const EdgeInsets.only(left: 20, top: 5),
       margin: const EdgeInsets.symmetric(horizontal: 20),
       decoration: BoxDecoration(
-          border: Border.all(
-            color: const Color.fromARGB(255, 160, 160, 147),
-          ),
-          borderRadius: BorderRadius.circular(10)),
+        border: Border.all(
+          color: const Color.fromARGB(255, 160, 160, 147),
+        ),
+        borderRadius: BorderRadius.circular(10),
+      ),
       child: Center(
         child: TextFormField(
           controller: controller,
@@ -133,13 +123,77 @@ class _AdminLoginState extends State<AdminLogin> {
             return null;
           },
           decoration: InputDecoration(
-              border: InputBorder.none,
-              hintText: hintText,
-              hintStyle: const TextStyle(
-                color: Color.fromARGB(255, 160, 160, 147),
-              )),
+            border: InputBorder.none,
+            hintText: hintText,
+            hintStyle: const TextStyle(
+              color: Color.fromARGB(255, 160, 160, 147),
+            ),
+          ),
         ),
       ),
     );
+  }
+
+  Widget _buttonWidget(VoidCallback onTap) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 12),
+        margin: const EdgeInsets.symmetric(horizontal: 20),
+        width: MediaQuery.sizeOf(context).width,
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: const Center(
+          child: Text(
+            'Login',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  loginAdmin() {
+    FirebaseFirestore.instance.collection('Admin').get().then((snapshot) {
+      // ignore: avoid_function_literals_in_foreach_calls
+      snapshot.docs.forEach((result) {
+        if (result.data()['id'] != userNameController.text.trim()) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text(
+                'Your name is not correct',
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+          );
+        } else if (result.data()['password'] !=
+            passController.text.trim()) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text(
+                'Your password is not correct',
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 18,
+                ),
+              ),
+            ),
+          );
+        } else {
+          Route route =
+              MaterialPageRoute(builder: (context) => const AddQuizScreen());
+          Navigator.pushReplacement(context, route);
+        }
+      });
+    });
   }
 }
